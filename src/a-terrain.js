16c852662046fa47c7b232b74dadf4fec3dae75d
Anselm Hook <anselm@hook.org>
2018-04-10 17:56:16 -0700

    helloworld example runs however at lod > 12 something is wrong :-( - tiles are loading and yet the display is not showing them - scale issue? not totally clear... this uses the orbit control.

--- src/a-terrain.js
+++ src/a-terrain.js
@@ -19,162 +19,60 @@ AFRAME.registerComponent('a-terrain', {
            lon: {type: 'number', default:    0},     // longitude
            lod: {type: 'number', default:    1},     // this is computed but left here since having a separate parent bucket/schema is a hassle
      elevation: {type: 'number', default:    1},     // height above ground
-      observer: {type: 'string', default: "camera"}  // id of an observer if any
+      observer: {type: 'string', default:   ""}      // id of camera or cameraRig - if there is not one then the lod has to be manually specified
   },
 
   ///
-  /// This component may be instanced more than once.
-  /// TODO data is conserved between instances but the back end server must be the same for all instances right now.
+  /// No need for more than one copy of component
   /// 
-  multiple: true,
+  multiple: false,
 
-  ///
-  /// TBD
-  /// 
-  /// remove: function () { },
+  // internal list of tiles for sweeper convenience
+  tiles: {},
 
-  ///
-  /// TBD
-  ///
-  /// update: function (oldData) { },
-
-  ///
-  /// TBD
-  ///
-  /// tick: function (t) { },
-
-  ///
-  /// TBD
-  /// 
-  /// pause: function () { },
-
-  ///
-  /// TBD
-  ///
-  /// play: function () { },
-
+  // delay for startup - TODO probably a more elegant way to latch tick()
+  refreshState: 0,
 
   ///
   /// Init
   ///
   init: function() {
-
-    // Wait for tile engine
+    this.refreshState = 0;
     TileServer.instance().ready( unused => {
-      // update once prior to any events so that there is something to see even if not perfect
-      this.updateTiles();
-      // listen to tiles being loaded for clearing backdrop
-      this.el.addEventListener("a-tile:visible", evt => {
-        alert(0); // WHYYYY TODO
-        this.sweepTiles(evt);
-      });
+      this.refreshState = 1;
     });
   },
 
-  markTiles: function() {
-    // TODO still debating the right approach here
-    // mark all tiles to age them out - tiles with an age of zero are fresh
-    if(!this.elements) this.elements = [];
-    this.elements.forEach(element => {
-      element.marked = element.marked > 0 ? element.marked + 1 : 1;
-    });
-  },
-
-  sweepTiles: function() {
-
-    // TODO still debating the right approach
-    // test: don't sweep till all tiles are complete - unsure if this will work because what if a tile never loads? also this list gets long...
-    let dirty = 0;
-    if(!this.elements) return;
-    this.elements.forEach(element => {
-      if(element.incomplete) dirty++;
-    });
-
-    if(dirty) return;
-
-    // sweep all old tiles
-    // TODO this should happen again after the system settles 
-    this.elements.forEach(element => {
-      if(!element.marked) {
-      //  console.log("visible " + element.id);
-      }
-      else if(element.marked > 0) {
-        element.setAttribute("visible",false);
-      }
-      else if(element.marked > 5) {
-        // actually delete them TODO
-      }
-    });
-  },
-
-  updateTiles: function() {
-
-    // re-estimate lod
-    this.data.lod = TileServer.instance().elevation2lod(this.data.elevation);
-
-    // mark all tiles for garbage collector
-    this.markTiles();
-
-    // test - just fetch one tile - unused
-    if(false) {
-      this.updateTile(this.data);
+  ///
+  /// tick at 60fps
+  ///
+  tick: function() {
+    // Ready?
+    if(!this.refreshState) {
       return;
     }
-
-    // ask tile server for facts about a given latitude, longitude, lod
-    let scheme = TileServer.instance().scheme_elaborate(this.data);
-
-    // the number of tiles to fetch in each direction is a function of the camera fov (45') and elevation over the size of a tile at current lod
-    let count = Math.floor(this.data.elevation / scheme.width_tile_lat) + 1;
-
-    // render enough tiles to cover the degrees visible - regardless of current lod - however it depends on the camera fov being 45'
-    for(let i = -count;i<count+1;i++) {
-      for(let j = -count;j<count+1;j++) {
-        // TODO this is also imperfect; there is a chance of a numerical error - it would be nice to be able to ask for tiles by index as well as by lat/lon
-        let scratch = { lat:this.data.lat + scheme.degrees_lat * i, lon:this.data.lon + scheme.degrees_lon * j, lod:this.data.lod, radius:this.data.radius };
-        // hack terrible code TODO cough forever loop
-        while(scratch.lon < -180) scratch.lon += 360;
-        while(scratch.lon >= 180) scratch.lon -= 360;
-        while(scratch.lat < -90) scratch.lat += 180;
-        while(scratch.lat >= 90) scratch.lat -= 180;
-        // make tile
-        this.updateTile(scratch);
-      }
-    }
-
-    // this.sweepTiles();
-  },
-
-  updateTile: function(data) {
-    // find tile by uuid that would cover requested point latitude, longitude, lod
-    let scheme = TileServer.instance().scheme_elaborate(data);
-    let uuid = scheme.uuid;
-    let element = this.el.querySelector("#"+uuid);
-    if(element) {
-      element.setAttribute("visible",true);
-      // issue this when visible
-      element.emit("a-tile:visible", {lat:data.lat, lon: data.lon, lod:data.lod,id:uuid }, false);
-    } else {
-      // if not found then ask a tile to build itself in such a way that it covers the given latitude, longitude, lod
-      element = document.createElement('a-entity');
-      element.incomplete = 1;
-      element.setAttribute('id',uuid);
-      element.setAttribute('a-tile',{lat:data.lat,lon:data.lon,lod:data.lod,radius:data.radius});
-      this.el.appendChild(element);
-      if(this.elements)this.elements.push(element);
-    }
-    element.marked = 0;
-  },
-
-  tick: function() {
+    // Update level of detail based on viewing mode
     this.updateView();
+    // Sweep old tiles if any
+    this.sweepTiles();
+  },
+
+  ///
+  /// Update view based on view mode
+  ///
+  updateView: function() {
+    if(this.data.observer.length > 0) {
+      this.updateView_Observer();
+    } else {
+      this.updateView_Origin();
+    }
   },
 
   ///
   /// Generate surface tiles to provide a consistent view from the viewpoint of the supplied target or camera
+  /// In this style of view the observer could interpenetrate mountains
   ///
-
-  updateView: function() {
+  updateView_Observer: function() {
 
     // Find observer again every frame since it may change or just return
     let observer = this.el.sceneEl.querySelector("#"+this.data.observer);
@@ -191,7 +89,8 @@ AFRAME.registerComponent('a-terrain', {
     // How far is the target from the globe in the model distance?
     let model_distance = this.el.object3D.position.distanceTo( this.observer_position );
 
-    // The observer does not know how high above the ellipsoid the current terrain elevation is - pick a number for now TODO ponder
+    // The observer does not know how high above the ellipsoid the current terrain elevation is - pick a number for now
+    // TODO ponder
     let ground_value = 500;
 
     // Right now we know the world radius ... code could be refactored to not need to know this TODO
@@ -201,7 +100,6 @@ AFRAME.registerComponent('a-terrain', {
     this.data.elevation = model_distance * world_radius / this.data.radius - world_radius + ground_value;
 
     // Recover latitude and longitude from observer
-
     this.data.lat = -observer.object3D.rotation.x * 180.0 / Math.PI;
     this.data.lon = observer.object3D.rotation.y * 180.0 / Math.PI;
 
@@ -209,9 +107,156 @@ AFRAME.registerComponent('a-terrain', {
     if(this.data.lat > 85) this.data.lat = 85;
     if(this.data.lat < -85) this.data.lat = -85;
 
-    // Generate appropriate tiles for this distance
+    // Generate appropriate tiles for this viewing vantage point
     this.updateTiles();
   },
+
+  ///
+  /// Update the view for an optional case where the planet surface is moved to 0,0,0 to make it easier for developers
+  ///
+
+  updateView_Origin: function() {
+
+    let data = this.data;
+
+    // rotate the world such that the supplied lat, lon is at 0,0
+
+    let obj = this.el.object3D;
+    obj.rotation.set(0,0,0);
+
+    var q = new THREE.Quaternion();
+    q.setFromAxisAngle( new THREE.Vector3(0,1,0), THREE.Math.degToRad(-data.lon) );
+    obj.quaternion.premultiply(q);
+    q.setFromAxisAngle( new THREE.Vector3(1,0,0), THREE.Math.degToRad(data.lat) );
+    obj.quaternion.premultiply(q);
+
+    // deal with distance and tiles
+    let world_radius = TileServer.instance().getRadius();
+    let world_circumference = TileServer.instance().getCircumference();
+
+    // estimate level of detail ... 
+    data.lod = TileServer.instance().elevation2lod(data.elevation);
+
+    let adjust_globe_elevation_relative_to_origin = function(groundValue) {
+      // the world surface should touch the origin
+      let distance = (world_radius+groundValue+data.elevation)*data.radius/world_radius;
+      // move earth surface here
+      obj.position.set(0,0,-distance);
+  //    console.log("for elevation " + data.elevation + " the lod is " + data.lod + " and ground is at " + groundValue + " rad="+data.radius + " distance=" + distance );
+    };
+
+
+   // quick set to avoid delays even if stale - ground level is not known yet but waiting for it causes visible delays
+    //if(data.groundLatched) {
+    //  adjust_globe_elevation_relative_to_origin(data.ground);
+    //}
+
+    // get ground height (has a delay)
+    TileServer.instance().getGround(data,groundValue => {
+      if(!groundValue || groundValue <0) groundValue = 0;
+      data.ground = groundValue;
+      data.groundLatched = true;
+      adjust_globe_elevation_relative_to_origin(groundValue);
+    });
+
+    // Generate appropriate tiles for this viewing vantage point
+    this.updateTiles();
+  },
+
+  ///
+  /// Given some facts about a desired longitude and latitude and level of detail generate some tiles here
+  /// TODO the assumption here is that the player is looking straight down and the camera has an fov of 45' - this assumption is wrong.
+  ///
+
+  updateTiles: function() {
+
+    // re-estimate level of detail given an elevation
+    this.data.lod = TileServer.instance().elevation2lod(this.data.elevation);
+
+    // ask tile server for facts about a given latitude, longitude, lod
+    let scheme = TileServer.instance().scheme_elaborate(this.data);
+
+    // the number of tiles to fetch in each direction is a function of the camera fov (45') and elevation over the size of a tile at current lod
+    let count = Math.floor(this.data.elevation / scheme.width_tile_lat) + 1;
+
+    // TODO improve view strategy - render enough tiles to cover the degrees visible - regardless of current lod - however it depends on the camera fov being 45'
+    for(let i = -count;i<count+1;i++) {
+      for(let j = -count;j<count+1;j++) {
+        // TODO this is sloppy; there is a chance of a numerical error - it would be better to be able to ask for tiles by index as well as by lat/lon
+        let scratch = { lat:this.data.lat + scheme.degrees_lat * i, lon:this.data.lon + scheme.degrees_lon * j, lod:this.data.lod, radius:this.data.radius };
+        // hack terrible code TODO cough forever loop
+        while(scratch.lon < -180) scratch.lon += 360;
+        while(scratch.lon >= 180) scratch.lon -= 360;
+        while(scratch.lat < -90) scratch.lat += 180;
+        while(scratch.lat >= 90) scratch.lat -= 180;
+        // make tile
+        this.updateOrCreateTile(scratch);
+      }
+    }
+
+  },
+
+  ///
+  /// Satisfy that a given tile exists
+  ///
+
+  updateOrCreateTile: function(data) {
+    // find tile by uuid that would cover requested point latitude, longitude, lod
+    let scheme = TileServer.instance().scheme_elaborate(data);
+    let uuid = scheme.uuid;
+    let element = this.el.querySelector("#"+uuid);
+    if(element) {
+      element.setAttribute("visible",true);
+      // not used - issue this when visible
+      // element.emit("a-tile:visible", {lat:data.lat, lon: data.lon, lod:data.lod,id:uuid }, false);
+    } else {
+      // if not found then ask a tile to build itself in such a way that it covers the given latitude, longitude, lod
+      element = document.createElement('a-entity');
+      element.setAttribute('id',uuid);
+      element.setAttribute('a-tile',{lat:data.lat,lon:data.lon,lod:data.lod,radius:data.radius});
+      this.el.appendChild(element);
+    }
+    this.tiles[scheme.uuid] = element;
+  },
+
+  ///
+  /// Hide tiles that are not interesting if they are occluded by interesting tiles
+  ///
+
+  sweepTiles: function() {
+
+    // return immediately if there are any tiles anywhere that are not fully loaded
+    let scope = this;
+    let ready = 1;
+
+    Object.keys(scope.tiles).forEach( (key) => {
+      let element = scope.tiles[key];
+      if(element.complete != 1) {
+        ready = 0;
+      }
+    });
+
+    // not ready do do any sweeping
+    if(!ready) {
+      return;
+    }
+
+    // sweep other tiles that are not at current lod
+
+    Object.keys(scope.tiles).forEach((key) => {
+      let element = scope.tiles[key];
+      if(element.complete != 1) {
+        console.error("An incomplete tile managed to get through the sweeper " + element.id);
+        return;
+      }
+      if(element.complete_lod != scope.data.lod && element.getAttribute("visible") != false) {
+        element.setAttribute("visible",false); 
+      }
+    });
+
+  },
+
+
 
 });
 
--- src/a-tile.js
+++ src/a-tile.js
@@ -19,14 +19,20 @@ AFRAME.registerComponent('a-tile', {
   },
   init: function () {
     let data = this.data;
+    if(!this.el)return;
+    // mark the lod for an outside observer
+    this.el.complete_lod = data.lod;
+    // mark as not fully loaded - cannot delete anything that has not passed this point
+    this.el.complete = 0;
+    // asynchronously begin loading process
     TileServer.instance().ready( unused => {
       TileServer.instance().produceTile(data,scheme => {
         // show tile
         this.el.setObject3D('mesh',scheme.mesh);
         // mark as complete
-        this.el.incomplete = 0;
-        // publish a general message that this tile is visible
-        this.el.emit("a-tile:visible", {lat:data.lat, lon: data.lon, lod:data.lod, id:this.el.id }, false);
+        this.el.complete = 1;
+        // not used - publish a general message that this tile is visible
+        // this.el.emit("a-tile:visible", {lat:data.lat, lon: data.lon, lod:data.lod, id:this.el.id }, false);
         // TODO it would be nice to know better if there were buildings without triggering an error
         if(scheme.lod < 15) return;
         let building = document.createElement('a-entity');
